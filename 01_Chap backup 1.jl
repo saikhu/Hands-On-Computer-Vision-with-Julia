88a8a74b8222ddfd5a915e7db96435d17ffac5de
Saikhu <asimsaikhu@gmail.com>
2022-03-08 10:10:52 +0900

    new

--- 01_Chap backup 1.jl
+++
@@ -1,8 +0,0 @@
-using Images, ImageView
-img = load("horse.jpg")
-# imshow(img)
-using ImageSegmentation
-seeds = [(CartesianIndex(126, 81), 1), (CartesianIndex(93, 255), 2), (CartesianIndex(213, 97), 3)]
-segments = seeded_region_growing(img, seeds)
-
-imshow(map(i->segment_mean(segments,i), labels_map(segments)));
